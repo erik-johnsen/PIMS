8322e0a490101a87f7c72c587b483deff2f8d3d3
Erik Johnsen <141843146+erik-johnsen@users.noreply.github.com>
2024-02-19 14:28:27 +0100

    created random id function and made everything OOP

--- js/app.js
+++ js/app.js
@@ -6,14 +6,20 @@ const nameInput = document.getElementById('form_name')
 const manufacturerInput = document.getElementById('form_manufacturer')
 const expirationInput = document.getElementById('form_expiration')
 const locationInput = document.getElementById('form_location')
-const quantityInput = document.getElementById('form_show-quantity')
+const quantityInput = document.getElementById('form_amount-to-add')
+const displayID = document.getElementById('form_show-id')
+const form = document.querySelector('.form-container')
 
 // Form Logic DOM
 const closeButton = document.querySelector('.close-button')
 const openButton = document.querySelector('.open-button')
 const addStorageWindow = document.querySelector('.add-storage')
 
-let allDataArray = JSON.parse(localStorage.getItem("userInfo")) || []
+const tbody = document.getElementById('tbody')
+
+let allDataArray = JSON.parse(localStorage.getItem("medicine")) || []
+
+let allRandomIDs = JSON.parse(localStorage.getItem("AllIDs")) || []
 
 
 
@@ -27,63 +33,99 @@ closeButton.addEventListener('click', ()=> {
 openButton.addEventListener('click', () => {
 	addStorageWindow.style.display = "flex"
 	openButton.style.display = "none"
+
+	form.reset()
+	Medicine.createID()
 })
 
 
-// Eventlistener to check if every field is filled out and then run addToStorage function
+class Medicine {
+	constructor(id, name, manufacturer, expiration, location, quantity) {
+		this.id = id,
+		this.name = name,
+		this.manufacturer = manufacturer,
+		this.expiration = expiration,
+		this.location = location,
+		this.quantity = quantity
+	}
+
+	static addMedicine() {
+		const newID = Medicine.createID()
+		const newMedicine = new Medicine (newID, nameInput.value, manufacturerInput.value, expirationInput.value, locationInput.value, quantityInput.value)
+
+		//pushes the new ID to a different array in local storage so i can make sure that no identical IDs can exist
+		allRandomIDs.push(newID)
+		localStorage.setItem("AllIDs", JSON.stringify(allRandomIDs))
+		return newMedicine
+		
+	}
+
+	// Function to create a random ID number, check if it already exist and also display it when the user is filling out the form
+	static createID() {
+		const newID = Math.floor(Math.random() * 1000000000);
+		if(allRandomIDs.includes(newID)) {
+			Medicine.createID()
+			return
+		}
+
+		displayID.textContent = newID
+		return newID
+
+	}
+}
+
+
+
+// Displays the data stored when website is loaded
+window.addEventListener('DOMContentLoaded', ()=> {
+	UI.displayData(allDataArray)
+})
+
+class UI {
+	static displayData = (data) => {
+		if(data) {
+			tbody.textContent = ""
+			
+			allDataArray.forEach(element => {
+				const trContainer = document.createElement('tr')
+				
+				const xContainer = document.createElement('td')
+				const idContainer = document.createElement('td')
+				const nameContainer = document.createElement('td')
+				const manufacturerContainer = document.createElement('td')
+				const expirationContainer = document.createElement('td')
+				const locationContainer = document.createElement('td')
+				const quantityContainer = document.createElement('td')
+			
+				tbody.appendChild(trContainer)
+				trContainer.append(xContainer, idContainer, nameContainer, manufacturerContainer, expirationContainer, locationContainer, quantityContainer)
+	
+				xContainer.textContent = "X"
+				idContainer.textContent = element.id
+				nameContainer.textContent = element.name
+				manufacturerContainer.textContent = element.manufacturer
+				expirationContainer.textContent = element.expiration
+				locationContainer.textContent = element.location
+				quantityContainer.textContent = element.quantity
+			})
+		}
+	}
+}
+
+
+// Eventlistener to check if every field is filled out and then run addMedicine, adds it to storage and runs displayData 
 submit.addEventListener('click', (e)=> {
 	e.preventDefault()
 	if(!nameInput.value.trim() || !manufacturerInput.value.trim() || !expirationInput.value.trim() || locationInput.value === "Select a location") {
 		console.log("fill it out!");
-	} else {addToStorage()}
+	} else {
+		
+		allDataArray.push(Medicine.addMedicine())
+		localStorage.setItem("medicine", JSON.stringify(allDataArray))
+		UI.displayData(allDataArray)
+
+		form.reset()
+		Medicine.createID()
+	}
 	
 })
-
-const addToStorage = () => {
-	const userInfo = {
-		id: "323333",
-		name: nameInput.value,
-		manufacturer: manufacturerInput.value,
-		expiration: expirationInput.value,
-		location: locationInput.value,
-		quantity: quantityInput.value
-	}
-	allDataArray.push(userInfo)
-	localStorage.setItem("medicine", JSON.stringify(allDataArray))
-}
-
-// Displays the data stored when website is loaded
-window.addEventListener('DOMContentLoaded', ()=> {
-	displayData(allDataArray)
-})
-
-const displayData = (data) => {
-	if(data) {
-		trContainer.textContent = ""
-
-		const trContainer = document.createElement('tr')
-		
-		data.forEach((element) => {
-			
-			const xContainer = document.createElement('td')
-			const idContainer = document.createElement('td')
-			const nameContainer = document.createElement('td')
-			const manufacturerContainer = document.createElement('td')
-			const expirationContainer = document.createElement('td')
-			const locationContainer = document.createElement('td')
-			const quantityContainer = document.createElement('td')
-
-			displayTable.appendChild(trContainer)
-			trContainer.append(xContainer, idContainer, nameContainer, manufacturerContainer, expirationContainer, locationContainer, quantityContainer)
-
-			xContainer.textContent = "X"
-			idContainer.textContent = element.id
-			nameContainer.textContent = element.name
-			manufacturerContainer.textContent = element.manufacturer
-			expirationContainer.textContent = element.expiration
-			locationContainer.textContent = element.location
-			quantityContainer.textContent = element.quantity
-		})
-	}
-}
-
